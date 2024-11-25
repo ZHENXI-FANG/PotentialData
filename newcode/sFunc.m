8eabdb4aad1564eca04a98c86e14a2f284b577b2
Fang <ZhenxiFang@csu.edu.cn>
2024-11-25 22:32:09 +0800

    更改sfunc

--- newcode/sFunc.m
+++ newcode/sFunc.m
@@ -1,7 +1,7 @@
 function [Value] = sFunc(diff,m,E,u)   %diff=p-q
    
     %D=sum(diff,2);
-    A= pinv(E) * diff';
+    A= pinv(E) * diff'; %这里是距离差与带宽矩阵相乘，下一步处理U/2,目前缺少这一环节，明天处理。
     B=diff.*A';
     temp=sum(B,2);   %行数相加
     Value=m./power(temp,0.8);
