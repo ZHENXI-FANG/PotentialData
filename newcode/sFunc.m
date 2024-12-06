aa4c8c05826f06482f257ee65ed12fe5e732a6cd
Fang <ZhenxiFang@csu.edu.cn>
2024-12-06 17:11:26 +0800

    指数函数

--- newcode/sFunc.m
+++ newcode/sFunc.m
@@ -4,13 +4,12 @@ function [Value] = sFunc(diff,m,E,u)   %diff=p-q
     B=diff.*A';
     temp=sum(B,2);   %行数相加
     Value=m./(power(10,-12)+power(temp,0.8));
-   
-   
-    % E_pinv=pinv(E);
-    % temp=diff*E_pinv;
-    % dist = sqrt(sum(temp.^2, 2));
-    % Value=m./(power(10,-12)+power(dist,0.8));
-        
+
+    % T=table(Value);
+    % filename='D:\PotentialData\data\2Dtest\1128test\Value.xlsx';
+    % writetable(T,filename);
+    % disp(['数据已经保存到',filename]);
+
     % C_matrix=diff * pinv(E).*diff ;
     % sum_matrix=sum(C_matrix,2);
     % Value=m./(power(10,-12)+power(sum_matrix,0.8));
