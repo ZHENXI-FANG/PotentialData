1ee8663a5b9789b15545e95110495a5e7ea317ef
Fang <ZhenxiFang@csu.edu.cn>
2025-01-08 16:53:42 +0800

    Comparing value of U,M,E

--- newcode/getParameters.m
+++ newcode/getParameters.m
@@ -19,7 +19,7 @@ function [m,q,t,ind] = getParameters(points, polyline)
     
     % Distance = vecnorm(Points - Polyline, 2, 3);  % 计算点集每个点到折点的欧氏距离
     % m=dmin.*sum(1./Distance,2);
-    m=2;
+    m=5;
 
     % if m(find(isnan(m))) == n
     %     %点与折点重合的情况，Distance为0，m为NaN
